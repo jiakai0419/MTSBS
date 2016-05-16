06f08f17d187b61d7b8d63dfa46eba8831c7de38
jiakai0419 <jiakai0419@gmail.com>
2016-05-16 09:23:12 +0800

    up

--- src/Transformers.hs
+++ src/Transformers.hs
@@ -10,3 +10,17 @@ import Control.Monad.Writer
 import Data.Maybe
 import qualified Data.Map as Map
 
+type Name = String
+
+data Exp = Lit Integer
+         | Var Name
+         | Plus Exp Exp
+         | Abs Name Exp
+         | App Exp Exp
+         deriving (Show)
+
+data Value = IntVal Integer
+           | FunVal Env Name Exp
+           deriving (Show)
+
+type Env = Map.Map Name Value
