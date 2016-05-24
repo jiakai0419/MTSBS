fcc32b9ba94b4749a2650d8addc15cde0fdbd58e
jiakai0419 <jiakai0419@gmail.com>
2016-05-24 15:59:28 +0800

    up

--- app/Main.hs
+++ app/Main.hs
@@ -8,4 +8,4 @@ exampleExp1 = Plus (Lit 1) (Abs "x" (Var "x"))
 exampleExp2 = Var "x"
 
 main :: IO ()
-main = putStrLn . show . runEval3 Map.empty . eval3 $ exampleExp
+main = putStrLn . show . runEval4 Map.empty 0 . eval4 $ exampleExp
--- src/Transformers.hs
+++ src/Transformers.hs
@@ -32,25 +32,34 @@ type Env = Map.Map Name Value
 
 type Eval4 a = ReaderT Env (ErrorT String (StateT Integer Identity)) a
 
-runEval4 :: Env -> Integer -> Eval4 -> (Either String a, Integer)
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
 runEval4 env st ev = runIdentity (runStateT (runErrorT (runReaderT ev env)) st)
 
--- eval4 :: Exp -> Eval4 Value
--- eval4 (Lit i) = return $ IntVal i
--- eval4 (Var n) = do env <- ask
---                    case Map.lookup n env of
---                      Nothing -> throwError $ "undefined variable: " ++ n
---                      Just val -> return val
--- eval4 (Plus e1 e2) = do val1 <- eval4 e1
---                         val2 <- eval4 e2
---                         case (val1, val2) of
---                           (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
---                           _ -> throwError "type error in addition"
--- eval4 (Abs n e) = do env <- ask
---                      return $ FunVal env n e
--- eval4 (App e1 e2) = do val1 <- eval4 e1
---                        val2 <- eval4 e2
---                        case val1 of
---                          FunVal env' n body -> local (const (Map.insert n val2 env')) (eval4 body)
---                          _ -> throwError "type error in application"
+tick :: (Num s, MonadState s m) => m ()
+tick = do st <- get
+          put (st + 1)
+
+eval4 :: Exp -> Eval4 Value
+eval4 (Lit i) = do tick
+                   return $ IntVal i
+eval4 (Var n) = do tick
+                   env <- ask
+                   case Map.lookup n env of
+                     Nothing -> throwError $ "undefined variable: " ++ n
+                     Just val -> return val
+eval4 (Plus e1 e2) = do tick
+                        val1 <- eval4 e1
+                        val2 <- eval4 e2
+                        case (val1, val2) of
+                          (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                          _ -> throwError "type error in addition"
+eval4 (Abs n e) = do tick
+                     env <- ask
+                     return $ FunVal env n e
+eval4 (App e1 e2) = do tick
+                       val1 <- eval4 e1
+                       val2 <- eval4 e2
+                       case val1 of
+                         FunVal env' n body -> local (const (Map.insert n val2 env')) (eval4 body)
+                         _ -> throwError "type error in application"
 
